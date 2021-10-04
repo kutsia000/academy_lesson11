57d0a4bcaf292dfe3049007e0ed1bd428090b672
giorgi_kutsia <giokucia1@gmail.com>
2021-10-04 17:52:25 +0400

    added functions

--- scripts/main.js
+++ scripts/main.js
@@ -1,22 +1,30 @@
 // 1
 let cars = ["audi","bmw", "lexus", "volkswagen", "porsche", "audi"];
 
-for(let i=1;i<cars.length;i++){
-    console.log(cars[i]);
+function getCars() {
+    for(let i=1;i<cars.length;i++){
+        console.log(cars[i]);
+    }
 }
+
+getCars();
 
 console.log();
 
 // 2
 let names = ["george","martha","abe","david","kate","Joe","John"];
 
-for(let i=0;i<names.length;i++){
-    if(names[i].length>3) console.log(names[i]);
+let getNames = function () {
+    for(let i=0;i<names.length;i++){
+        if(names[i].length>3) console.log(names[i]);
+    }
+    console.log();
+    for(let i of names){
+        if(i.length<=3) console.log(i);
+    }
 }
-console.log();
-for(let i of names){
-    if(i.length<=3) console.log(i);
-}
+
+getNames();
 
 console.log();
 
@@ -31,3 +39,44 @@ while(y<numbers.length){
     if(numbers[y]!==0&&numbers[y]%2==1) console.log("odd: "+numbers[y]);
     y++;
 }
+
+console.log();
+
+var person = {
+    firstname: "john",
+    lastname: "doe",
+    age: 50,
+    eyecolor: "blue"
+};
+
+console.log(person.eyecolor);
+
+console.log();
+
+let data = ["sandro","ana","beqa","nino","nika","salome","lasha"];
+
+getData=()=>{
+    for(let i=0;i<data.length;i++){
+        console.log(data[i]);
+    }
+    console.log();
+    for(let c of data){
+        console.log(c);
+    }
+    console.log();
+    let c = 0;
+    while(c<data.length){
+        console.log(data[c]);
+        c++;
+    }
+    console.log();
+    data.forEach(element => {
+        console.log(element);
+    });
+}
+
+getData();
+
+if(data.length===5&&data[data.length-1]==="nika") console.log("masivis raodenoba: 5 da bolo elementia nika");
+
+if(data.length===7||data[0]==="sandro") console.log(`მასივის რაოდენობა: ${data.length} და პირველი ელემენტია ${data[0]}`);
